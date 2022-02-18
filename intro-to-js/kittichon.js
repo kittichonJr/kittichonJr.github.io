865b7f01b7de0a7d44ace9bc1304ec341d93d5b7
kittichon phitakkiri <mikesatur27@gmail.com>
2022-02-18 14:12:46 +0700

    add new exercise agecal

--- intro-to-js/kittichon.js
+++ intro-to-js/kittichon.js
@@ -1,25 +1,3 @@
-// create h1 text
-const myname = document.createElement('h1');
-myname.setAttribute("id","myname");
-const myname_txt = document.createTextNode('My name is');
-myname.appendChild(myname_txt);
-let body = document.getElementsByTagName('body')[0];
-body.appendChild(myname);
-
-// button Ask my name
-const btnAskname = document.createElement('button');
-const btnAskTxt = document.createTextNode('Ask My Name');
-btnAskname.appendChild(btnAskTxt);
-btnAskname.setAttribute("onclick","askMyName()");
-body.appendChild(btnAskname);
-
-// button Hello World
-const btnHello = document.createElement('button');
-const btnHelloTxt = document.createTextNode('Hello World');
-btnHello.appendChild(btnHelloTxt);
-btnHello.setAttribute("onclick","helloWorld()");
-body.appendChild(btnHello);
-
 // function
 function askMyName(){
     const name = prompt("What is your name?");
@@ -32,9 +10,15 @@ function helloWorld(){
     const h1message = document.getElementById("myname").innerHTML;
     alert(h1message + " โซ่เถอะนะน้อง ทองมันแพง");
 }
-
-//style
-body.setAttribute("style","width:80%;margin:auto;display:flex;justify-content:center;flex-direction:column;align-items:cetner;text-align:center;background-color:#27B5B8;")
-btnAskname.setAttribute("style","height:100px; margin-bottom:15px; background-color:#EBCA1A; font-size:18px");
-btnHello.setAttribute("style","height:100px; background-color:#EBCA1A; font-size:18px");
-myname.setAttribute("style","color:white;")
+function calAge(){
+    let inAge = prompt("When you was born?","2000");
+    let currentDate = new Date();
+    let calAge = Number(currentDate.getFullYear()) - Number(inAge);
+    alert("ยินดีด้วย นายอยู่ในประเทศนี้มา"+calAge+"ปีแล้ว");
+}
+function loadProfile(){
+    const name = "kittichon";
+    const province = "Krung Thep Maha Nakhon";
+    document.getElementById("my-name").innerHTML = name;
+    document.getElementById("my-province").innerHTML = province;
+}
